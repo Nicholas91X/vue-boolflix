d5011fa67359b0623e823ec31282c3a14c829d1c
Nicholas <romitinicholas@gmail.com>
2021-12-21 16:33:51 +0100

    milestone 3 senza flags

--- src/components/commons/FilmCard.vue
+++ src/components/commons/FilmCard.vue
@@ -1,20 +1,34 @@
 <template>
     <div id="film-card">
-        <h3>{{titolo}}</h3>
-        <h3>{{titoloOriginale}}</h3>
-        <h4>{{lingua}}</h4>
-        <h4>{{voto}}</h4>
+        <img :src="http+img" alt="">
+        <div id="informazioni">
+            <h3>{{titolo}}</h3>
+            <h3>{{titoloOriginale}}</h3>
+            <h4>{{lingua}}</h4>
+            <h4 id="vote">{{stelle = Math.ceil(voto / 2)}} <i class="colored" v-for="(stella,index) in stelle" :key="index">&#9733;</i><i v-for="stella in 5 - Math.ceil(voto / 2)" :key="stella">&#9734;</i>
+            </h4>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
     name: "FilmCard",
+    data() {
+        return {
+            stelle: []
+        }
+    },
     props: {
+        img: String,
         titolo: String,
         titoloOriginale: String,
         lingua: String,
-        voto: Number
+        voto: Number,
+        http: String
+    },
+    mounted() {
+        console.log(this.stelle)
     }
 }
 </script>
@@ -23,8 +37,28 @@ export default {
     #film-card {
         width: 200px;
         height: 300px;
-        background-color: aqua;
+        background-color: rgba(0, 0, 0, 0.75);
         margin: 10px;
         text-align: center;
+        position: relative;
+        &:hover img {
+            opacity: 0;
+        }
+        img {
+            width: 100%;
+            height: 100%;
+            position: absolute;
+            top: 0;
+            left: 0;
+        }
+        #informazioni {
+            color: white;
+            h4 {
+                font-size: 24px;
+                i.colored {
+                    color: goldenrod;
+                }
+            }
+        }
     }
 </style>
--- src/components/macro/Main.vue
+++ src/components/macro/Main.vue
@@ -2,11 +2,11 @@
     <main>
         <section>
             <h2>Films</h2>
-            <FilmCard v-for="(movie,index) in film" :key="index" :titolo="film[index].title" :titoloOriginale="film[index].original_title" :lingua="film[index].original_language" :voto="film[index].vote_average"/>
+            <FilmCard v-for="(movie,index) in film" :key="index" :http="indirizzo" :img="film[index].poster_path" :titolo="film[index].title" :titoloOriginale="film[index].original_title" :lingua="film[index].original_language" :voto="film[index].vote_average"/>
         </section>
         <section>
             <h2>Serie</h2>
-            <FilmCard v-for="(movie,index) in serie" :key="index" :titolo="serie[index].name" :titoloOriginale="serie[index].original_name" :lingua="serie[index].original_language" :voto="serie[index].vote_average"/>
+            <FilmCard v-for="(movie,index) in serie" :key="index" :http="indirizzo" :img="serie[index].poster_path" :titolo="serie[index].name" :titoloOriginale="serie[index].original_name" :lingua="serie[index].original_language" :voto="serie[index].vote_average"/>
         </section>
     </main>
 </template>
@@ -15,9 +15,14 @@
 import FilmCard from "../commons/FilmCard.vue"
 export default {
     name: "Main",
+    data() {
+        return {
+            indirizzo: "https://image.tmdb.org/t/p/w342"
+        }
+    },
     props: {
         film: Array,
-        serie: Array
+        serie: Array,
     },
     components: {
         FilmCard
